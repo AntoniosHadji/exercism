1a1ef0d4c8c13d7fe0b43f87fffb07e70134e166
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-09-14 17:38:25 -0400

    refactor default case

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -13,36 +13,39 @@ def hey(phrase):
     c = re.search(r'\?\s*$', phrase)
     # test all caps and spaces,  ends in ? plus 0 or more spaces
     d = re.search(r'^[A-Z\s]{2,}\?\s*$', phrase)
+
     if b or (c and not d):
         return 'Sure.'
+
     if c and d:
         return "Calm down, I know what I'm doing!"
 
-    # multiline
-    e = re.search(r'\n', phrase)
-    # does phrase contain ?
-    f = re.search(r'\?', phrase)
-    # does phrase end with whitespace
-    g = re.search(r'\s+$', phrase)
-    # does phrase start with whitespace
-    j = re.search(r'^\s+', phrase)
     # only numbers commas whitespace
     h = re.search(r'^[0-9\,\s]+$', phrase)
-    # contains ? in middle of statement
-    k = re.search(r'.+\?.+', phrase)
-    # ends in a period (statement)
-    m = re.search(r'.+\.$', phrase)
-    # ends in an exclamation mark
-    n = re.search(r'\!$', phrase)
-    # contains lower case letters
-    o = re.search(r'[a-z]', phrase)
-
-    if m or e or h or j or k or (not f and g) or (n and o):
-        return 'Whatever.'
-
-    # numbers and capital letters
-    # i = re.search(r'^[A-Z\s0-9\,]{2,}\!*\s*$', phrase)
     # everything other than lower case
     i = re.search(r'^[^a-z]{2,}\!*\s*$', phrase)
-    if i:
+    if i and not h:
         return 'Whoa, chill out!'
+
+    # # multiline
+    # e = re.search(r'\n', phrase)
+    # # does phrase contain ?
+    # f = re.search(r'\?', phrase)
+    # # does phrase end with whitespace
+    # g = re.search(r'\s+$', phrase)
+    # # does phrase start with whitespace
+    # j = re.search(r'^\s+', phrase)
+    # # contains ? in middle of statement
+    # k = re.search(r'.+\?.+', phrase)
+    # # ends in a period (statement)
+    # m = re.search(r'.+\.$', phrase)
+    # # ends in an exclamation mark
+    # n = re.search(r'\!$', phrase)
+    # # contains lower case letters
+    # o = re.search(r'[a-z]', phrase)
+
+    # if m or e or j or k or (not f and g) or (n and o):
+    #     return 'Whatever.'
+
+    # these tests above are superflous.
+    return 'Whatever.'
