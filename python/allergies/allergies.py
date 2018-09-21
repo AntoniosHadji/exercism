95d42b38debc025ca4244e65a8802f6f12319b16
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-09-21 18:59:59 -0400

    submitted allergies

---
+++ python/allergies/allergies.py
@@ -0,0 +1,25 @@
+class Allergies(object):
+    test_score = -1
+    allergies = ['eggs', 'peanuts', 'shellfish', 'strawberries', 'tomatoes',
+                 'chocolate', 'pollen', 'cats']
+
+    def __init__(self, score):
+        self.test_score = score
+
+    def is_allergic_to(self, item):
+        if self.test_score == 0:
+            return False
+        if item in self.lst:
+            return True
+        else:
+            return False
+
+    @property
+    def lst(self):
+        result = []
+        score = self.test_score % 256
+        for s in range(7, -1, -1):
+            if score >= 2**s:
+                result.append(self.allergies[s])
+                score -= 2**s
+        return result
