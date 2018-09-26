e11ea1cb657a4f0bf1a4481bf63e4a94a6e9c62b
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-09-26 12:39:35 -0400

    final version with bitwise operators

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,24 +1,54 @@
+'''
+Given a person's allergy score, determine whether or not they're allergic to a
+given item, and their full list of allergies.
+
+An allergy test produces a single numeric score which contains the information
+about all the allergies the person has (that they were tested for).
+
+The list of items (and their value) that were tested are:
+
+    eggs (1)
+    peanuts (2)
+    shellfish (4)
+    strawberries (8)
+    tomatoes (16)
+    chocolate (32)
+    pollen (64)
+    cats (128)
+
+So if Tom is allergic to peanuts and chocolate, he gets a score of 34.
+
+Now, given just that score of 34, your program should be able to say:
+
+    Whether Tom is allergic to any one of those allergens listed above.
+    All the allergens Tom is allergic to.
+
+Note: a given score may include allergens not listed above
+(i.e. allergens that score 256, 512, 1024, etc.).
+Your program should ignore those components of the score.
+For example, if the allergy score is 257,
+your program should only report the eggs (1) allergy.
+'''
+
+
 class Allergies(object):
-    test_score = -1
-    allergies = ['eggs', 'peanuts', 'shellfish', 'strawberries', 'tomatoes',
-                 'chocolate', 'pollen', 'cats']
 
     def __init__(self, score):
-        self.test_score = score
+        self._test_score = score
+        # score values are 1, 2, 4, 8, 16, 32, 64, 128
+        self._allergies = ['eggs', 'peanuts', 'shellfish', 'strawberries',
+                           'tomatoes', 'chocolate', 'pollen', 'cats']
 
     def is_allergic_to(self, item):
-        if self.test_score == 0:
-            return False
-        if item in self.lst:
-            return True
-        else:
-            return False
+        return item in self.lst
 
     @property
     def lst(self):
         result = []
-        score = self.test_score % 256
-        for s in range(7, -1, -1):
-            if score & 2**s:
-                result.append(self.allergies[s])
+        for i, f in enumerate(self._allergies):
+            # shifting 1 by i creates 1,2,4,8...2**n sequence
+            # bitwise & = 1 if _test_score is divisible by sequence value
+            # this works because scores are same pattern as sequence
+            if self._test_score & (1 << i):
+                result.append(f)
         return result
