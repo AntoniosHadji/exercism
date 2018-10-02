57e8bcbd8580683bc3151ec7c565efa63c8d462a
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-10-02 08:12:13 -0400

    refactor

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,7 +1,3 @@
 def sum_of_multiples(limit, multiples):
-    result = set()
-
-    for m in multiples:
-        result = result.union({n for n in range(m, limit, m) if n % m == 0})
-
+    result = {n for m in multiples for n in range(m, limit, m)}
     return sum(result)
