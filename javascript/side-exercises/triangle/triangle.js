f17c2539caca4c2ab38eecf8d9091f4c0be70cce
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-10-20 15:37:32 -0400

    js: completed exercise

---
+++ javascript/side-exercises/triangle/triangle.js
@@ -0,0 +1,28 @@
+export default class Triangle {
+  constructor (a, b, c) {
+    this.a = a
+    this.b = b
+    this.c = c
+  }
+
+  kind () {
+    if (!(this.a + this.b >= this.c) ||
+        !(this.a + this.c >= this.b) ||
+        !(this.b + this.c >= this.a) ||
+        this.a <= 0 || this.b <= 0 || this.c <= 0) {
+      throw new Error('invalid triangle')
+    }
+
+    if (this.a === this.b && this.a === this.c && this.b === this.c) {
+      return 'equilateral'
+    }
+
+    if (this.a === this.b || this.a === this.c || this.b === this.c) {
+      return 'isosceles'
+    }
+
+    if (this.a !== this.b && this.a !== this.c && this.b !== this.c) {
+      return 'scalene'
+    }
+  }
+}
