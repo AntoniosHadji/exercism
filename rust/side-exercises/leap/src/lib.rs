1c63f2c904e78253bf03f8e300151759e7e9a9ec
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-10-20 14:54:07 -0400

    rust: refactor

--- rust/side-exercises/leap/src/lib.rs
+++ rust/side-exercises/leap/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn is_leap_year(year: i32) -> bool {
-    let four: bool            = (year % 4)   == 0;
-    let hundred: bool         = (year % 100) == 0;
-    let four_hundred: bool    = (year % 400) == 0;
+    let four            = (year % 4)   == 0;
+    let hundred         = (year % 100) == 0;
+    let four_hundred    = (year % 400) == 0;
 
     // return not needed in rust no semicolon returns value
     four && !hundred || four_hundred
