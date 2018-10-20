c7eb9287b27801c24c4862d5b85881cb12e8e0a8
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-10-20 09:36:19 -0400

    python: refactored

--- python/side-exercises/rna-transcription/rna_transcription.py
+++ python/side-exercises/rna-transcription/rna_transcription.py
@@ -1,14 +1,12 @@
+'''
+https://docs.python.org/3.6/library/stdtypes.html?highlight=translate#str.translate
+https://docs.python.org/3.6/library/stdtypes.html?highlight=translate#str.maketrans
+'''
 import re
 
 
 def to_rna(dna_strand):
-    translation = {'G': 'C',
-                   'C': 'G',
-                   'T': 'A',
-                   'A': 'U'
-                   }
-
     if re.search('^[GCTA]+$', dna_strand):
-        return dna_strand.translate(str.maketrans(translation))
+        return dna_strand.translate(str.maketrans('GCTA', 'CGAU'))
     else:
         raise ValueError("DNA strand invalid, empty or incorrect nucleotide")
