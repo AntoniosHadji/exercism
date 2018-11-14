d8cfd9f03cc9654352c7daedeb52646806d4b757
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-11-13 21:59:42 -0500

    python: refactor wip

--- python/side-exercises/isbn-verifier/isbn_verifier.py
+++ python/side-exercises/isbn-verifier/isbn_verifier.py
@@ -6,11 +6,18 @@ def verify(isbn):
     if not re.match(r'^\d{9}[0-9X]$', isbn):
         return False
 
-    result = 0
+    # How to account for X in this code?
+    # a = list(isbn)
+    # b = list(range(10, 0, -1))
+    # return sum([int(x) * y for x, y in zip(a, b)]) % 11 == 0
+
+    if 'X' in isbn:
+        result = 10
+        isbn = isbn[:-1] + '0'
+    else:
+        result = 0
+
     for i, c in enumerate(isbn):
-        if c == 'X':
-            result += 10 * (10 - i)
-        else:
-            result += int(c) * (10 - i)
+        result += int(c) * (10 - i)
 
     return result % 11 == 0
