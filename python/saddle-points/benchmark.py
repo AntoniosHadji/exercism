a09309a20ecb01b6de88cddc622feb0b30fc8c89
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-11-14 16:45:45 -0500

    python: completed anagram

--- python/saddle-points/benchmark.py
+++ python/saddle-points/benchmark.py
@@ -1,4 +1,15 @@
 import timeit
 
+setup = 'from saddle_points import saddle_points'
+cmd = 'saddle_points([[9, 8, 7], [5, 3, 2], [6, 6, 7]])'
 
-print(timeit.timeit('from saddle_points import saddle_points;saddle_points([[9, 8, 7], [5, 3, 2], [6, 6, 7]])', number=1000000))
+print(timeit.timeit(cmd, setup=setup, number=1000000))
+
+
+# original timing commit [:f587b86]
+# 5.84843750204891
+# 5.959444281004835
+
+# algorithm #2
+# 4.6083211319637485
+# 4.579655827023089
--- python/side-exercises/anagram/anagram.py
+++ python/side-exercises/anagram/anagram.py
@@ -1,2 +1,9 @@
 def find_anagrams(word, candidates):
-    pass
+    test = sorted(word.lower())
+    valid_candidates = [
+            candidate for candidate in candidates
+            if word.lower() != candidate.lower()
+            ]
+    return [candidate for candidate in valid_candidates
+            if test == sorted(candidate.lower())
+            ]
