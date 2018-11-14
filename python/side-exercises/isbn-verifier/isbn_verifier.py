0a06184ff0b97953ece4d403a6047ef2be658a9b
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-11-13 21:35:38 -0500

    python: isbn all tests passed

--- python/side-exercises/isbn-verifier/isbn_verifier.py
+++ python/side-exercises/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,16 @@
+import re
+
+
 def verify(isbn):
-    pass
+    isbn = re.sub(r'-', '', isbn)
+    if not re.match(r'^\d{9}[0-9X]$', isbn):
+        return False
+
+    result = 0
+    for i, c in enumerate(isbn):
+        if c == 'X':
+            result += 10 * (10 - i)
+        else:
+            result += int(c) * (10 - i)
+
+    return result % 11 == 0
