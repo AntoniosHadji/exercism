2cf6858107a3bcf4eaa7b35307105a14194aa030
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-11-17 17:42:42 -0500

    python: saddle point refactor wip

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -3,22 +3,19 @@ def saddle_points(matrix):
         return set()
 
     length = len(matrix[0])
-    for row in matrix:
-        if len(row) != length:
-            raise ValueError('irregular matrix')
-
     result = set()
 
     # enumerate rows of matrix
     for i, row in enumerate(matrix):
+        if len(row) != length:
+            raise ValueError('irregular matrix')
         # enumerate across columns of row
         for j, col in enumerate(row):
             # if value is max in row, it is potential saddle point
             if col == max(row):
-                target_value = matrix[i][j]
                 # loop through column values to find smaller value
                 for k in range(len(matrix)):
-                    if matrix[k][j] < target_value:
+                    if matrix[k][j] < col:
                         break
                 else:
                     # no smaller value found in column, value is saddle point
