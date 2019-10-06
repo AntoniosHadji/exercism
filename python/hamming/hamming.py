30272f056cf72328e7dc4feb0b98450e3f1244a4
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2019-10-06 16:47:39 -0400

    python: promote hamming to track excercise

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,10 +1,12 @@
 # better community solution
 # returns sum of True values (True == 1)
 #     return sum(i != j for i, j in zip(strand_a, strand_b))
+# expression inside sum is a generator exprssion
+# zip does not throw error if not equal length
 
 
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
-        raise ValueError('strands do match in length')
+        raise ValueError("strands do match in length")
 
     return len([c for i, c in enumerate(strand_a) if c != strand_b[i]])
