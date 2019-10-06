1dbcdd5823f9a1d7c2df633e7351d5a84627e2fd
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2019-10-06 18:53:40 -0400

    rust: bonus test grapheme clusters

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,3 +1,7 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    input.chars().rev().collect()
+    //  input.chars().rev().collect()
+    //  true to use extended graphemes
+    UnicodeSegmentation::graphemes(input, true).rev().collect()
 }
