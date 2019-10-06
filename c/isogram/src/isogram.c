11506836b4b83dda7bc4abdf8cf44838c95c9d6b
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2019-10-06 15:20:55 -0400

    c: isogram submit

--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
 
 bool is_isogram(const char phrase[])
 {
+  int alphabet[26] = {0};
+  int index;
+
+  for (unsigned long i=0; i < strlen(phrase); i++) {
+    if (phrase[i] == ' ' || phrase[i] == '-')
+      continue;
+
+    index = tolower(phrase[i]) - 'a';
+    alphabet[index] += 1;
+    if (alphabet[index] > 1) {
+      return 0;
+    }
+  }
+
   return 1;
 }
 
--- c/isogram/test/test_isogram.c
+++ c/isogram/test/test_isogram.c
@@ -17,50 +17,56 @@ void test_empty_string(void)
 
 void test_lower_case_only(void)
 {
-   TEST_IGNORE();               // delete this line to run test
+   // TEST_IGNORE();               // delete this line to run test
    TEST_ASSERT_TRUE(is_isogram("isogram"));
 }
 
 void test_duplicated_letter(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("eleven"));
 }
 
 void test_longest_known_isogram(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    TEST_ASSERT_TRUE(is_isogram("subdermatoglyphic"));
 }
 
 void test_duplicated_letter_mixed_case(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("Alphabet"));
 }
 
 void test_non_letter_char(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    TEST_ASSERT_TRUE(is_isogram("thumbscrew-japingly"));
 }
 
 void test_duplicated_non_letter_char(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    TEST_ASSERT_TRUE(is_isogram("Hjelmqvist-Gryb-Zock-Pfund-Wax"));
 }
 
 void test_multiple_whitespace(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    TEST_ASSERT_TRUE(is_isogram("Emily Jung Schwartzkopf"));
 }
 
 void test_duplicated_letter_within_word(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("accentor"));
+}
+
+void test_full_alphabet(void)
+{
+   // TEST_IGNORE();
+   TEST_ASSERT_TRUE(is_isogram("abcdefghijklmnopqrstuvwxyz"));
 }
 
 int main(void)
@@ -76,6 +82,7 @@ int main(void)
    RUN_TEST(test_duplicated_non_letter_char);
    RUN_TEST(test_multiple_whitespace);
    RUN_TEST(test_duplicated_letter_within_word);
+   RUN_TEST(test_full_alphabet);
 
    UnityEnd();
    return 0;
