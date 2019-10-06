2c7011366e3598898ffe9b70acda5aa650fb2513
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2019-10-06 19:06:07 -0400

    rust: grapheme simplification

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -3,5 +3,6 @@ use unicode_segmentation::UnicodeSegmentation;
 pub fn reverse(input: &str) -> String {
     //  input.chars().rev().collect()
     //  true to use extended graphemes
-    UnicodeSegmentation::graphemes(input, true).rev().collect()
+    // UnicodeSegmentation::graphemes(input, true)...
+    input.graphemes(true).rev().collect()
 }
