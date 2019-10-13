172137eebd35647518392c1d9fd6ac6ea9ed5dd8
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2019-10-13 18:55:11 -0400

    go: 2nd submit hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -2,21 +2,26 @@ package hamming
 
 import "errors"
 
+// Distance calculates hamming distance of two genome strings
 func Distance(a, b string) (int, error) {
+	// if runes are important, get runes as slice that can be indexed
+	// a_runes := []rune(a)
+	// b_runes := []rune(b)
 
-  if len(a) != len(b) {
-    return -1, errors.New("mismatch in string lengths");
-  }
+	if len(a) != len(b) {
+		return 0, errors.New("mismatch in string lengths")
+	}
 
-  result := 0;
-  for i, _ := range a {
-    // _ would be type rune
-    // indexing into string returns a byte
-    // a[i], b[i] are unicode code points, not actual text character
-    if a[i] != b[i] {
-      result += 1;
-    }
-  }
+	result := 0
+	// , c would return type rune in c
+	for i := range a {
+		// if a_runes[i] != b_runes[i] {
+		// indexing into string returns a byte
+		// a[i], b[i] are unicode code points, not actual text character
+		if a[i] != b[i] {
+			result++
+		}
+	}
 
-  return result, nil;
+	return result, nil
 }
