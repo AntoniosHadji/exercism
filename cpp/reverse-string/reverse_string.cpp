ef557001225dfb2958aac1bc022ed20c30cc10c3
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2019-10-13 19:29:04 -0400

    c++: reverse_string

--- cpp/reverse-string/reverse_string.cpp
+++ cpp/reverse-string/reverse_string.cpp
@@ -1,5 +1,16 @@
 #include "reverse_string.h"
+#include <string>
+
+using namespace std;
 
 namespace reverse_string {
+
+  string reverse_string(string s) {
+    string result;
+    for (int i = s.length() - 1; i>=0 ; i--) {
+      result += s[i];
+    }
+    return result;
+  }
 
 }  // namespace reverse_string
