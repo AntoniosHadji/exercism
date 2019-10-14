208dfdd8d678c890d3322b073ba6f2a980817723
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2019-10-13 21:14:11 -0400

    python: clean comments

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -2,26 +2,10 @@ import string
 
 ALPHABET = string.ascii_lowercase
 CIPHER = string.ascii_lowercase[::-1]
-"""
-
-In [7]: t = str.maketrans(string.ascii_lowercase, string.ascii_lowercase[::-1])
-
-In [8]: "abc".translate(t)
-Out[8]: 'zyx'
-
-remove all unwanted characters with strip, then translate
-then split into groups of 5
-
-"""
 
 
 def encode(plain_text):
 
-    # t = str.maketrans(
-    #     string.ascii_lowercase + string.digits,
-    #     string.ascii_lowercase[::-1] + string.digits,
-    # )
-    # result = plain_text.lower().replace(" ", "").strip(string.punctuation).translate(t)
     result = ""
     for c in plain_text.lower():
 
