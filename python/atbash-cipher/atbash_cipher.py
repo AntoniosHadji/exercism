35620bb1cefa6ff8f97c92c237e5f0fcdbbba6b8
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2019-10-13 21:27:55 -0400

    python: atbash_cipher v2

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,31 +1,16 @@
 import string
 
-ALPHABET = string.ascii_lowercase
-CIPHER = string.ascii_lowercase[::-1]
-
 
 def encode(plain_text):
-
-    result = ""
-    for c in plain_text.lower():
-
-        if c in string.digits:
-            result += c
-
-        if c in ALPHABET:
-            result += CIPHER[ALPHABET.index(c)]
-
+    result = decode(plain_text)
     result = [result[i : i + 5] for i in range(0, len(result), 5)]
     return " ".join(result)
 
 
 def decode(ciphered_text):
-    result = ""
-    for c in ciphered_text.lower():
-        if c in string.digits:
-            result += c
-
-        if c in ALPHABET:
-            result += ALPHABET[CIPHER.index(c)]
-
-    return "".join(result)
+    t = str.maketrans(
+        string.ascii_lowercase[::-1] + string.digits,
+        string.ascii_lowercase + string.digits,
+        string.whitespace + string.punctuation,
+    )
+    return ciphered_text.lower().translate(t)
