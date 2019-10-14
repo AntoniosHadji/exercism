fb3bca6d001c0d1a1f4f1e0e507efcf059a69340
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2019-10-14 07:11:40 -0400

    c++: reverse_string v2

--- cpp/reverse-string/reverse_string.cpp
+++ cpp/reverse-string/reverse_string.cpp
@@ -1,16 +1,12 @@
 #include "reverse_string.h"
 #include <string>
-
-using namespace std;
+#include <algorithm>
 
 namespace reverse_string {
 
-  string reverse_string(string s) {
-    string result;
-    for (int i = s.length() - 1; i>=0 ; i--) {
-      result += s[i];
-    }
-    return result;
+  std::string reverse_string(std::string s) {
+    reverse(begin(s), end(s));
+    return s;
   }
 
 }  // namespace reverse_string
