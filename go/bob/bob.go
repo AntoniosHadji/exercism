e46f924e472a27316ecdba49adaedb9fa93f553a
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2019-10-20 19:56:55 -0400

    go: bob passing tests

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,15 +1,29 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package bob should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// bob is a chat bot
 package bob
 
-// Hey should have a comment documenting it.
+import "regexp"
+
+// Hey takes a comment and returns a response
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	var onlyWhitespace = regexp.MustCompile(`^\s+$`)
+	var question = regexp.MustCompile(`.+\?\s*$`)
+	var q_all_caps_with_whitespace = regexp.MustCompile(`^[A-Z\s]+\?\s*$`)
+	var numbers_comma_whitespace = regexp.MustCompile(`^[0-9,\s]+$`)
+	var everything_not_lower_case = regexp.MustCompile(`^[^a-z]+[\!\s]*$`)
+
+	if onlyWhitespace.MatchString(remark) || len(remark) == 0 {
+		return "Fine. Be that way!"
+	} else if q_all_caps_with_whitespace.MatchString(remark) {
+		return "Calm down, I know what I'm doing!"
+	} else if question.MatchString(remark) {
+		return "Sure."
+	} else if numbers_comma_whitespace.MatchString(remark) {
+		return "Whatever."
+	} else if everything_not_lower_case.MatchString(remark) {
+		return "Whoa, chill out!"
+	} else {
+		return "Whatever."
+	}
 }
+
+// https://golang.org/doc/effective_go.html#commentary
