c8d4bc40ca0ae4872fd0b98a43ddab19ae348b5d
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2019-10-29 19:01:29 -0400

    c: suggested improvement

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -1,22 +1,22 @@
+#include <stdio.h>
 #include <math.h>
 #include "armstrong_numbers.h"
 
 int isArmstrongNumber(int n) {
   int count = 1;
-  // save original number
+  // create working variable equal to n
   int num = n;
   // count number of digits
-  while (n > 9) {
+  while (num > 9) {
     count += 1;
-    n /= 10;
+    num /= 10;
   }
-
-  // reset n to original num
-  n = num;
+  // reset working variable to original n
+  num = n;
   int sum = 0;
   for (int i=0; i < count; i++) {
-    sum += pow(n % 10, count);
-    n /= 10;
+    sum += pow(num % 10, count);
+    num /= 10;
   }
-  return sum == num;
+  return sum == n;
 }
