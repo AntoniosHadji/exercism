603214a9dee4d89e3158ebcaad2b62176e52c75c
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2019-11-11 07:18:30 -0500

    python: replace list(map()) with list comprehension

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,6 +1,6 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        self.m = [list(map(int, e.split(" "))) for e in matrix_string.splitlines()]
+        self.m = [[int(n) for n in e.split()] for e in matrix_string.splitlines()]
 
     def row(self, index):
         return self.m[index - 1]
