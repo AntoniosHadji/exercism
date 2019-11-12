ddba199a9b5556fc3b6689a91410985a7961727b
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2019-11-12 07:11:56 -0500

    python: use regex instead of replace

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,8 +1,9 @@
 from collections import Counter
+import re
 
 
 def is_isogram(string):
-    s = string.lower().replace(" ", "").replace("-", "")
+    s = re.sub(r"[ -]", "", string.lower())
     c = Counter(s)
     # if sum of values == length of counter, each character only appears once
     return sum(c.values()) == len(c)
