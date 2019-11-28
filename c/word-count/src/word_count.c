d932b6388f66d8d2dc21fc7908faa1af1f22ff94
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2019-11-28 17:58:55 -0500

    c: word count pass all tests

--- c/word-count/src/word_count.c
+++ c/word-count/src/word_count.c
@@ -11,7 +11,7 @@
 #define NEW_WORD_FOUND 77
 #define NOT_A_WORD 88
 
-int increment_word_index(const char *word, word_count_word_t *words);
+int increment_word_index(char *word, word_count_word_t *words);
 
 int word_count(const char *input_text, word_count_word_t * words) {
   // clear to start with a known value
@@ -24,15 +24,15 @@ int word_count(const char *input_text, word_count_word_t * words) {
   // loop over input_text until end
   while ((c = tolower(*input_text++))) {
     // test for end of word
-    printf("c: [%c]\n", c);
+    // printf("c: [%c]\n", c);
     if (isspace(c) || (ispunct(c) && c != '\'')) {
-      printf("tested for space or punc: [%c]\n", c);
+      // printf("tested for space or punc: [%c]\n", c);
       // add or increment count
       int rc = increment_word_index(current_word, words);
       if (rc == EXCESSIVE_NUMBER_OF_WORDS) return EXCESSIVE_NUMBER_OF_WORDS;
       if (rc == NEW_WORD_FOUND) {
         unique_words++;
-        printf("unique count: %d\n", unique_words);
+        // printf("unique count: %d\n", unique_words);
       }
       memset(current_word, 0, sizeof(current_word));
       i = 0;
@@ -41,7 +41,7 @@ int word_count(const char *input_text, word_count_word_t * words) {
       if (strlen(current_word) > MAX_WORD_LENGTH) return EXCESSIVE_LENGTH_WORD;
       current_word[i++] = c;
       current_word[i] = '\0';
-      printf("current_word: %s\n", current_word);
+      // printf("current_word: %s\n", current_word);
     }
   }
 
@@ -52,13 +52,29 @@ int word_count(const char *input_text, word_count_word_t * words) {
   return unique_words;
 }
 
-int increment_word_index(const char *word, word_count_word_t *words) {
-  printf("increment_word_index: [%s]\n", word);
+int increment_word_index(char *word, word_count_word_t *words) {
+  //printf("increment_word_index: [%s]\n", word);
+  //printf("increment_word_index: [%c]\n", *word);
+  // unsigned long
+  // printf("length: [%lu]\n", strlen(word));
   if (*word == '\0') return NOT_A_WORD;
+  if (*word == '\'') {
+    // word begins with apostrophe, assume quoted
+    unsigned long i = 0;
+    // shift word forward 1 char to overwrite first apostrophe
+    // length - 2 to stop before last apostrophe
+    for (; i < (strlen(word) - 2); i++) {
+      word[i] = word[i+1];
+    }
+    // add new null to end string
+    word[i] = '\0';
+    // printf("increment_word_index post clean: [%s]\n", word);
+  }
+
   // loop over array of structs
   for (int i=0; i<MAX_WORDS; i++) {
     // if word == text, found match, return index
-    printf("word: [%s] text: [%s] count: [%d]\n", word, words[i].text, words[i].count);
+    // printf("word: [%s] text: [%s] count: [%d]\n", word, words[i].text, words[i].count);
     if (strcmp(word, words[i].text) == 0) {
       // found word
       words[i].count++;
@@ -72,7 +88,7 @@ int increment_word_index(const char *word, word_count_word_t *words) {
       }
       words[i].count = 1;
       // words[i].text[MAX_WORD_LENGTH + 1] = '\0';
-      printf("word: [%s] text: [%s] count: [%d]\n", word, words[i].text, words[i].count);
+      // printf("word: [%s] text: [%s] count: [%d]\n", word, words[i].text, words[i].count);
       return NEW_WORD_FOUND;
     }
 
