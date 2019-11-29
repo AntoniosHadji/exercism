3bfddf1d16f4ebf4fd0a01697ee8e9241afbaeab
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2019-11-28 20:57:20 -0500

    c:word count all tests passing v3

--- c/word-count/src/word_count.c
+++ c/word-count/src/word_count.c
@@ -6,8 +6,8 @@
 
 #include <stdio.h>
 
-#define NEW_WORD_FOUND 77
-#define NOT_A_WORD 88
+#define NEW_WORD_FOUND 1
+#define NOT_A_WORD -3
 
 int increment_word_index(char * word, word_count_word_t * words);
 
@@ -23,39 +23,45 @@ int word_count(const char * input_text, word_count_word_t * words) {
   // loop over input_text until end
   while ((c = tolower(*input_text++))) {
     // printf("c: [%c]\n", c);
+    // test for if this c is part of a quoted word, if so skip quotes
     if (c == '\'') {
       char pre = (input_text-2)[0];
       char post = (input_text)[0];
-      // test for if this c is part of a quoteed word
       if ((isspace(pre) && isalnum(post)) || (isalnum(pre) && isspace(post))) {
           continue;
       }
     }
+
     if (isalnum(c) || c == '\'') {
       //copy char to temp word
       if (strlen(current_word) > MAX_WORD_LENGTH) return EXCESSIVE_LENGTH_WORD;
       current_word[i++] = c;
-      // printf("current_word: %s\n", current_word);
     } else {
       // add or increment count
-      int rc = increment_word_index(current_word, words);
+      int rc = NOT_A_WORD;
+      if (*current_word != '\0') {
+        rc = increment_word_index(current_word, words);
+      }
       if (rc == EXCESSIVE_NUMBER_OF_WORDS) return EXCESSIVE_NUMBER_OF_WORDS;
       if (rc == NEW_WORD_FOUND) {
         unique_words++;
-        printf("unique count: %d\n", unique_words);
       }
       memset(current_word, 0, sizeof(current_word));
       i = 0;
     }
-
   }
   current_word[i] = '\0';
 
-  if (increment_word_index(current_word, words) == NEW_WORD_FOUND) {
-    unique_words++;
+  if (*current_word != '\0') {
+    if (increment_word_index(current_word, words) == NEW_WORD_FOUND) {
+      unique_words++;
+    }
   }
 
-  //printf("pre-return unique count: %d\n", unique_words);
+  printf("pre-return unique count: %d\n", unique_words);
+  for (int i=0; i<MAX_WORDS; i++) {
+    printf("text: [%s] count: [%d]\n", words[i].text, words[i].count);
+  }
   return unique_words;
 }
 
@@ -63,16 +69,14 @@ int increment_word_index(char * word, word_count_word_t * words) {
   //printf("increment_word_index: [%s]\n", word);
   // unsigned long
   // printf("length: [%lu]\n", strlen(word));
-  if (*word == '\0') return NOT_A_WORD;
 
   // loop over array of structs
   for (int i=0; i<MAX_WORDS; i++) {
     // if word == text, found match, return index
-    // printf("word: [%s] text: [%s] count: [%d]\n", word, words[i].text, words[i].count);
     if (strcmp(word, words[i].text) == 0) {
       // found word
       words[i].count++;
-      return 1;  // not a new word
+      return 0;  // not a new word
     }
     // if text is empty, end of word list
     if (*words[i].text == '\0') {
