3f01ee7a54b40af1880508209cfd51b130f32811
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2019-11-29 21:38:44 -0500

    c: word count submission

--- c/word-count/src/word_count.c
+++ c/word-count/src/word_count.c
@@ -1,18 +1,16 @@
 #include "word_count.h"
 #include <string.h>
-// strcmp
+// strcmp, strlen
 #include <ctype.h>
-// isspace, ispunct, tolower
+// isspace, tolower, isalnum
 
-#include <stdio.h>
-
-#define NEW_WORD_FOUND 1
 
 int increment_word_index(char * , word_count_word_t * );
 void clear_array(word_count_word_t * );
 
+
 int word_count(const char * input_text, word_count_word_t * words) {
-  // clear to start with a known value
+  // clear to start with a known empty array
   clear_array(words);
 
   int unique_words = 0;
@@ -24,36 +22,37 @@ int word_count(const char * input_text, word_count_word_t * words) {
   while ((c = tolower(*input_text++))) {
     // return if EXCESSIVE_LENGTH_WORD
     if (strlen(current_word) > MAX_WORD_LENGTH) return EXCESSIVE_LENGTH_WORD;
+    // return if MAX_WORDS already found
+    if (unique_words == MAX_WORDS) return EXCESSIVE_NUMBER_OF_WORDS;
 
-    // test if this c is part of a quoted word, if so skip quotes
+    // test if c is part of a quoted word, if so skip quotes
     if (c == '\'') {
-      char pre = (input_text-2)[0];
-      char post = (input_text)[0];
+      // input_text has already been incremented to next char
+      char pre = *(input_text-2);  // char prior to c
+      char post = *input_text;     // next char after c
       // previous character is space, next char is alphanumeric == opening quote
-      // previos character is alphanumeric, next char is space == closing  quote
+      // previous character is alphanumeric, next char is space == closing  quote
       if ((isspace(pre) && isalnum(post)) || (isalnum(pre) && isspace(post))) {
           continue;
       }
     }
 
+    // if c is alphanumeric, or apostrophe between two letters
     if (isalnum(c) || c == '\'') {
-      if (unique_words == MAX_WORDS) return EXCESSIVE_NUMBER_OF_WORDS;
-      //copy char to temp word
+      //add char to current word
       current_word[i++] = c;
     } else {
-      // count word
-      if (*current_word != '\0') {
-        unique_words += increment_word_index(current_word, words);
-      }
-
+      // c is a word boundary character
+      // count word if new, or increment existing word
+      unique_words += increment_word_index(current_word, words);
+      // reset variables for next word
       memset(current_word, 0, sizeof(current_word));
       i = 0;
     }
   }
 
-  if (*current_word != '\0') {
-      unique_words += increment_word_index(current_word, words);
-  }
+  // count/increment last word
+  unique_words += increment_word_index(current_word, words);
 
   return unique_words;
 }
@@ -66,21 +65,23 @@ void clear_array(word_count_word_t * words) {
 }
 
 int increment_word_index(char * word, word_count_word_t * words) {
-  // loop over array of structs
-  for (int i=0; i<MAX_WORDS; i++) {
-    // if word == text, found match, return index
-    if (strcmp(word, words[i].text) == 0) {
-      // found word
-      words[i].count++;
-      return 0;  // did not find a new word
-    }
-    // if text is empty, end of word list, add new word if word not empty
-    if (*words[i].text == '\0') {
-      strncpy(words[i].text, word, MAX_WORD_LENGTH);
-      words[i].count = 1;
-      return 1;  // found new word
+  if (strlen(word) > 0) {  // this also worked != '\0') {
+    // loop over array of structs
+    for (int i=0; i<MAX_WORDS; i++) {
+      // if word == text, found match, return index
+      if (strcmp(word, words[i].text) == 0) {
+        // found word
+        words[i].count++;
+        return 0;  // did not find a new word
+      }
+      // if text is empty, end of word list, add new word if word not empty
+      if (*words[i].text == '\0') {
+        strncpy(words[i].text, word, MAX_WORD_LENGTH);
+        words[i].count = 1;
+        return 1;  // found new word
+      }
     }
   }
-  // this should never be reached
-  return 1000;
+  // if strlen <= 0 no word, return and do not increment any word
+  return 0;
 }
