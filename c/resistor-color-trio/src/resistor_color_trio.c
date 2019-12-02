fc4b36ef8e919ed40a1ffc3fa21dcf4c8aa18307
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2019-12-01 19:20:36 -0500

    c: resistor-color-trio submitted

--- c/resistor-color-trio/src/resistor_color_trio.c
+++ c/resistor-color-trio/src/resistor_color_trio.c
@@ -1,8 +1,19 @@
 #include "resistor_color_trio.h"
 #include <math.h>
 
+resistor_value_t color_code(resistor_band_t cc[]) {
+  resistor_value_t result;
 
-uint16_t color_code(resistor_band_t cc[]) {
+  result.value = (cc[0] * 10 + cc[1]) * (int) pow(10, cc[2]);
 
-  return (cc[0] * 10 + cc[1]) * (uint16_t) pow(10, cc[2]);
+  if (result.value < 1000) {
+    result.unit = OHMS;
+  } else {
+    result.unit = KILOOHMS;
+    result.value /= KILOOHMS;
+  }
+
+  return result;
 }
+
+
--- c/resistor-color-trio/src/resistor_color_trio.h
+++ c/resistor-color-trio/src/resistor_color_trio.h
@@ -3,25 +3,30 @@
 #ifndef RESISTOR_COLOR_TRIO_H
 #define RESISTOR_COLOR_TRIO_H
 
-#define BLACK 0
-#define BROWN 1
-#define RED 2
+#define BLACK  0
+#define BROWN  1
+#define RED    2
 #define ORANGE 3
 #define YELLOW 4
-#define GREEN 5
-#define BLUE 6
+#define GREEN  5
+#define BLUE   6
 #define VIOLET 7
-#define GREY 8
-#define WHITE 9
+#define GREY   8
+#define WHITE  9
 
-typedef uint16_t resistor_band_t;
+#define OHMS        1
+#define KILOOHMS 1000
+
+// Using int instead of uint16_t because values are greater than 2^16 (65536)
+typedef int resistor_band_t;
 
 typedef struct {
-  uint16_t value;
-  int units;
-} resistor_value_t;
+  int value;
+  int unit;
+} resistor_value_t ;
 
-uint16_t color_code(resistor_band_t[]);
+
+resistor_value_t color_code(resistor_band_t[]);
 #endif
 
 
