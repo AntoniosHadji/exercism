d11488d469c589d28c9c30a8515d12635ad38308
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2019-12-08 09:36:24 -0500

    c: word_count test strtok function
    strtok is destructive to string, requires temp string
    getting extraneous ending characters when running through strtok (.)
    still need to go through character by character to convert to lower

--- c/word-count/src/word_count.c
+++ c/word-count/src/word_count.c
@@ -11,34 +11,52 @@ int word_count(const char * input_text, word_count_word_t * words) {
   memset(words, 0, MAX_WORDS * sizeof(word_count_word_t));
 
   int unique_words = 0;
-  char current_word[MAX_WORD_LENGTH + 1] = {0};
-  char c;
-  int i = 0;
+  //char current_word[MAX_WORD_LENGTH + 1] = {0};
+  //  char c;
+  //  int i = 0;
+  // create temp input because strtok is destructive
+  int input_length = strlen(input_text);
+  char temp_input[input_length];
+  strncpy(temp_input, input_text, input_length);
 
-  // loop over input_text until end
-  while ((c = tolower(*input_text++))) {
+  char * delim = " :,!&@$%^&.\n";
+  char *token = strtok(temp_input, delim);
+  while(token) {
+    puts(token);
     // return if EXCESSIVE_LENGTH_WORD
-    if (strlen(current_word) > MAX_WORD_LENGTH) return EXCESSIVE_LENGTH_WORD;
+    if (strlen(token) > MAX_WORD_LENGTH) return EXCESSIVE_LENGTH_WORD;
     // return if MAX_WORDS already found
     if (unique_words == MAX_WORDS) return EXCESSIVE_NUMBER_OF_WORDS;
 
-    // if c is alphanumeric, or apostrophe
-    if (isalnum(c) || c == '\'') {
-      //add char to current word
-      current_word[i++] = c;
-    }
+    unique_words += increment_word_index(token, words);
 
-    // c is a word boundary character, or finished with input_text
-    if ((!isalnum(c) && c != '\'') || *input_text == '\0') {
-      if (*current_word != '\0') {
-        // count word if new, or increment existing word
-        unique_words += increment_word_index(current_word, words);
-        // reset variables for next word
-        memset(current_word, 0, sizeof(current_word));
-        i = 0;
-      }
-    }
+    token = strtok(NULL, delim);
   }
+
+//  // loop over input_text until end
+//  while ((c = tolower(*input_text++))) {
+//    // return if EXCESSIVE_LENGTH_WORD
+//    if (strlen(current_word) > MAX_WORD_LENGTH) return EXCESSIVE_LENGTH_WORD;
+//    // return if MAX_WORDS already found
+//    if (unique_words == MAX_WORDS) return EXCESSIVE_NUMBER_OF_WORDS;
+//
+//    // if c is alphanumeric, or apostrophe
+//    if (isalnum(c) || c == '\'') {
+//      //add char to current word
+//      current_word[i++] = c;
+//    }
+//
+//    // c is a word boundary character, or finished with input_text
+//    if ((!isalnum(c) && c != '\'') || *input_text == '\0') {
+//      if (*current_word != '\0') {
+//        // count word if new, or increment existing word
+//        unique_words += increment_word_index(current_word, words);
+//        // reset variables for next word
+//        memset(current_word, 0, sizeof(current_word));
+//        i = 0;
+//      }
+//    }
+//  }
 
   return unique_words;
 }
