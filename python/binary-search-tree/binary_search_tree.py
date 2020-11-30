d23951024f42a50eb38fa0c0badf76ee0fb6959d
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2020-11-30 10:09:23 -0500

    updated binary search tree

--- python/binary-search-tree/binary_search_tree.py
+++ python/binary-search-tree/binary_search_tree.py
@@ -1,4 +1,4 @@
-class TreeNode(object):
+class TreeNode:
     def __init__(self, data, left, right):
         self.data = data
         self.left = left
@@ -9,7 +9,7 @@ class TreeNode(object):
         return fmt.format(self.data, self.left, self.right)
 
 
-class BinarySearchTree(object):
+class BinarySearchTree:
     def __init__(self, tree_data: list):
         # root must be set outside of recursion
         self.root = TreeNode(tree_data[0], None, None)
@@ -22,20 +22,20 @@ class BinarySearchTree(object):
     def sorted_data(self):
         return self.traverse(self.root, [])
 
-    def insert(self, node: type(TreeNode), data: str):
+    def insert(self, node: TreeNode, data: str):
         if data <= node.data:
-            if node.left:
-                self.insert(node.left, data)
-            else:
+            if node.left is None:
                 node.left = TreeNode(data, None, None)
-        else:
-            if node.right:
-                self.insert(node.right, data)
             else:
+                self.insert(node.left, data)
+        else:
+            if node.right is None:
                 node.right = TreeNode(data, None, None)
+            else:
+                self.insert(node.right, data)
 
-    def traverse(self, node: type(TreeNode), result: list):
-        if node:
+    def traverse(self, node: TreeNode, result: list):
+        if node is not None:
             result = self.traverse(node.left, result)
             result.append(node.data)
             result = self.traverse(node.right, result)
