d0f5f99b091683fc302bd5e42951f47fd532df6a
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2020-11-30 19:51:23 -0500

    updated binary search logic
    based on python3 bisect algo

--- python/binary-search/binary_search2.py
+++ python/binary-search/binary_search2.py
@@ -1,18 +1,22 @@
 def binary_search(list_of_numbers, number):
     lo = 0
-    hi = len(list_of_numbers) - 1
-    while lo <= hi:
+    hi = len(list_of_numbers)
+    while lo < hi:
         index = (lo + hi) // 2
         middle_number = list_of_numbers[index]
         if number == middle_number:
             return index
 
         if number < middle_number:
-            hi = index - 1
+            hi = index
             # return binary_search(list_of_numbers[:index], number)
         else:
             lo = index + 1
             # return index + binary_search(list_of_numbers[index:], number)
-    if lo > hi:
+
+    if lo == hi:
         raise ValueError("Number not found.")
-    return index
+    try:
+        return index
+    except NameError:
+        raise ValueError("Empty array.")
