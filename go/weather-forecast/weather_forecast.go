097882d9d1926c01700acb7a595ff91960ddaa19
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2023-01-09 16:40:04 -0500

    go: weather forecast

---
+++ go/weather-forecast/weather_forecast.go
@@ -0,0 +1,14 @@
+// Package weather provides weather forescasts
+package weather
+
+// CurrentCondition reports the current weather conditions
+var CurrentCondition string
+
+// CurrentLocation records the location for which the conditions are valid.
+var CurrentLocation string
+
+// Forecast takes condition and location strings and returns string report
+func Forecast(city, condition string) string {
+	CurrentLocation, CurrentCondition = city, condition
+	return CurrentLocation + " - current weather condition: " + CurrentCondition
+}
